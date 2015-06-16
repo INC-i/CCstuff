dae517bc5b5de80ca6cd5ea02816570c4b2771b7
root <root@devadmin.club>
2015-06-16 11:52:38 +0900

    add index on tables

---
+++ sql/create_index.sql
@@ -0,0 +1,8 @@
+'INDEX_all_asn':
+CREATE INDEX 'index_all_asn' ON 'all_asn' ('ASN_MINIMUM','ASN_MAXIMUM')
+'INDEX_all_ipv4_mini':
+CREATE INDEX 'index_all_ipv4_mini' ON 'all_ipv4_mini' ('ADDR_MINIMUM','ADDR_MAXIMUM')
+'INDEX_all_ipv4':
+CREATE INDEX 'index_all_ipv4' ON 'all_ipv4' ('ADDR_MINIMUM','ADDR_MAXIMUM')
+'INDEX_iso3166_1':
+CREATE INDEX 'index_iso3166_1' ON 'iso3166_1' ('CC2','NAME_EN')
